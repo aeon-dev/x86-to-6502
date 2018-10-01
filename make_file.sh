6ffa01fdae99a273fb66a44090a190f3f1583f4d
Robin Degen <robindegen@gmail.com>
2018-10-01 20:20:05 +0200

    Add cmake bootstrap shell script.

--- make_file.sh
+++ make_file.sh
@@ -1,7 +1,41 @@
 #! /bin/bash
 
-~/llvm-build/bin/clang++ -std=c++1z -c -O3 -o- -Wall -Wextra -m32 -march=i386 -ggdb -S $1 > $1.x86.asm
-cat $1.x86.asm | ~/x86-to-6502-build/x86-to-6502 > $1.6502.asm
-cat $1.6502.asm | sed -e "/^\t\..*$/d" > $1.asm
-~/Downloads/TMPx_v1.1.0-STYLE/linux-x86_64/tmpx $1.asm
+#echo ARGS: $@
+
+PARAMS=""
+
+while (( "$#" )); do
+  case "$1" in
+    -g)
+      shift
+      ;;
+    -S)
+      shift
+      ;;
+    -O2)
+      shift
+      ;;
+    -o)
+      OUTPUT_FILE=$2
+      shift 2
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      INPUT_FILE="$1"
+      shift
+      ;;
+  esac
+done
+
+#echo "INPUT FILE: $INPUT_FILE.blah"
+#echo "OUTPUT FILE: $OUTPUT_FILE"
+
+clang++ -std=c++17 -c -g -O2 -o- -Wall -Wextra -m32 -march=i386 -ggdb -S $INPUT_FILE > $INPUT_FILE.x86.asm
+cat $INPUT_FILE.x86.asm | /home/robindegen/x86-to-6502/build/x86-to-6502 > $INPUT_FILE.6502.asm
+#cat $INPUT_FILE.6502.asm | sed -e "/^\t\..*$/d" > $OUTPUT_FILE
+cat $INPUT_FILE.6502.asm > $OUTPUT_FILE
+#~/Downloads/TMPx_v1.1.0-STYLE/linux-x86_64/tmpx $1.asm
 
